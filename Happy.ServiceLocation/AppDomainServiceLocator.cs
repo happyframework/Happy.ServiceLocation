e9854b626d74bb546f85196beb42d87909c91262
dgw <shijiucha@qq.com>
2014-05-13 10:37:13 +0800

    dgw：初始化项目。

---
+++ Happy.ServiceLocation/AppDomainServiceLocator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Practices.ServiceLocation;
+
+using Happy.Utils;
+
+namespace Happy.ServiceLocation
+{
+    /// <summary>
+    /// 基于AppDomain的实现，只用于测试环境。
+    /// </summary>
+    public sealed class AppDomainServiceLocator : ServiceLocatorImplBase
+    {
+        /// <inheritdoc />
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            return AppDomain.CurrentDomain.CreateConcreteDescendentInstances(serviceType);
+        }
+
+        /// <inheritdoc />
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            return AppDomain.CurrentDomain.CreateConcreteDescendentInstance(serviceType);
+        }
+    }
+}
